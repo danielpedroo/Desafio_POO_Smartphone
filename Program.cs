a6a86df95a2ea1254ef406e0b678b9c7d9e97fde
danielpedroo <danielpapereiraJV@gmail.com>
2024-01-14 23:05:47 -0300

    Desafio Celular POO - Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "1198756231", modelo: "Nokia positivo P28", imei: "212854844961", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Linkedin");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone iphone = new Iphone(numero: "119546328", modelo: "Iphone 15 Pro MAX", imei: "6541965411", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
